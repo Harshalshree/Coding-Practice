775a81c6dd5943bdd88b7b464c3295f342db726a
Harshal Shree <50375501+Harshalshree@users.noreply.github.com>
2021-03-23 15:17:14 +0530

    Update subsetsum.cpp

--- GeeksForGeeks/DP/subsetsum.cpp
+++ GeeksForGeeks/DP/subsetsum.cpp
@@ -24,7 +24,7 @@ int main(){
     }
     for(int i = 1; i < n+1; i++){
         for(int j = 1; j < sum+1; j++){
-            if(arr[i] <= j){
+            if(arr[i-1] <= j){
                 dp[i][j] = dp[i-1][j-arr[i]] || dp[i-1][j];
             }
             else{
