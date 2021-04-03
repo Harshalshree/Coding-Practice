c41888bb396858d2b9799d416e9c1a779da8e821
Harshalshree <harshalshree03@gmail.com>
2021-04-03 20:53:34 +0530

    Subset Sum fix | MinSubsetSumDiff Added (WIP)

--- GeeksForGeeks/DP/MinimumSubsetSumDiff.cpp
+++ GeeksForGeeks/DP/MinimumSubsetSumDiff.cpp
@@ -1,17 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool dp[1000][1000];
+bool dp[100][100];
 
 int main(){
     int n;
     cin >> n;
     int arr[n];
+    int sum = 0;
+
     for(int i = 0; i < n; i++){
         cin >> arr[i];
+        sum = sum + arr[i];
     }
-    int sum;
-    cin >> sum;
+
     for(int i = 0; i < n+1; i++){
         for(int j = 0; j < sum+1; j++){
             if(i == 0){
@@ -25,6 +27,18 @@ int main(){
     for(int i = 1; i < n+1; i++){
         for(int j = 1; j < sum+1; j++){
             if(arr[i-1] <= j){
+                dp[i][j] = dp[i-1][j-arr[i]] || dp[i-1][j];
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+
+    for(int i = 1; i < n+1; i++){
+        for(int j = 1; j < sum+1; j++){
+            if(arr[i-1] <= j){
                 dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
             }
             else{
@@ -33,12 +47,12 @@ int main(){
         }
     }
 
-    for (int i = 0; i <= n; i++)
-     {
-       for (int j = 0; j <= sum; j++)
-          printf ("%4d", dp[i][j]);
-       printf("\n");
-     }
 
+    int ans = INT_MAX;
+    for(int i = 0; i < sum/2; i++){
+        if(dp[n][i] == true){
+            ans = min(ans, sum - 2 * i);
+        }
+    }
     cout << dp[n][sum] << endl;
 }
